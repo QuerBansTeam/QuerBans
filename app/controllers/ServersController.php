e8272e24d5cfef405182c6d4b3979e16d070f412
Karol Szuster <karolsz9898@gmail.com>
2017-11-17 10:42:02 +0100

    Add actions to Server controller

--- app/controllers/ServersController.php
+++ app/controllers/ServersController.php
@@ -11,8 +11,49 @@ class ServersController extends ControllerBase {
     }
 
     public function indexAction() {
+        $serversEntries = ServerInfo::find();
+        $serversIps = [];
 
-        
+        foreach ($serversEntries as $server) {
+            $serversIps[] = $server->address;
+        }
+
+        $this->view->ipArray = $serversIps;
     }
 
+    public function getAction() {
+        $this->view->disable();
+
+        if ($this->request->isPost() && $this->request->isAjax()) {
+            $ip = $this->dispatcher->getParam('ip');
+
+            if (!strlen($ip)) {
+                echo json_encode([
+                    "error" => "Empty IP address",
+                ]);
+                return;
+            }
+
+            if (!IsValidIp($ip)) {
+                echo json_encode([
+                    "error" => "Invalid IP address",
+                ]);
+                return;
+            }
+
+            $portPos = strpos($ip, ':');
+            try {
+                $ServerInfo = new ServerQuery(substr($ip, 0, $portPos), intval(substr($ip, $portPos + 1)));
+                echo json_encode([
+                    "server" => $ServerInfo->getServerInfo(),
+                    "players" => $ServerInfo->getPlayers(),
+                    "rules" => $ServerInfo->getRules(),
+                ]);
+            } catch(Exception $e) {
+                echo json_encode([
+                    "error" => $e->getMessage(),
+                ]);
+            }
+        }
+    }
 }
