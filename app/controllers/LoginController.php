3ee4f3ab2c1a4e20e52589a004ea1e8952e9d6e0
Karol Szuster <karolsz9898@gmail.com>
2017-11-26 20:17:21 +0100

    Add basic login mechanism

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -3,15 +3,36 @@
 use Phalcon\Mvc\Controller;
 use Phalcon\Mvc\Model\Query\Builder;
 
-class BansController extends ControllerBase {
+class LoginController extends ControllerBase {
 
     public function loginAction() {
+        $this->view->disable();
 
         if ($this->request->isPost()) {
             $login      =   $this->request->getPost('login');
             $password   =   $this->request->getPost('password');
 
-            //$user =
+            $user = Admins::findFirst([
+                "username" => $login,
+            ]);
+
+            if ($user) {
+                if ($this->security->checkHash($password, $user->password)) {
+                    $this->session->set('username', $login);
+                }
+            } else {
+                $this->security->hash(rand());
+            }
         }
+        $this->response->redirect();
+    }
+
+    public function logoutAction() {
+        $this->view->disable();
+
+        if ($this->session->has('username')) {
+            $this->session->destroy();
+        }
+        $this->response->redirect();
     }
 }
